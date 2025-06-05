d1e22b13fc497a53e68341f76892c5cbc0a87cb3
shinya <shinya.gp@gmail.com>
2025-06-05 15:41:10 +0900

    Unified FeatureID Generation Compatible with MVT, OGR, and JavaScript(#4)

--- fude.rb
+++ fude.rb
@@ -4,8 +4,19 @@ require 'digest'
 pref = ENV['PREF']
 basename = ENV['BASENAME']
 
-def hash64(str)
-  Digest::SHA256.hexdigest(str)[0, 16].to_i(16)
+# SHA256由来のハッシュ値を、53bit符号なし整数（JavaScriptで安全に扱える範囲）に縮小する
+# これは、MVT以外でのFeature ID利用を想定したもので、下記2つの観点で実装している
+# 1. JavaScriptで安全に扱える最大整数値(Number.MAX_SAFE_INTEGER = 2^53 - 1)を超えないようにする
+# 2. OGR の int64（2^63 - 1）を超えないようにする
+# 3. Tippecanoe の整数ID（int64）でオーバーフローを起こさないようにする
+# - 64bitのハッシュからIDを生成するため一意性が高い
+# - 53bit以下に収めることで、JavaScript でも安全に扱える
+# - Tippecanoe や OGR（int64）でもオーバーフローを起こさない
+# 可読性の高い ID 文字列（global_id）を元にSHA256 の先頭64bitを整数化し、
+# その値を MAX_SAFE_INTEGER で剰余して、JavaScript・FGB・MVT すべてに対応可能な範囲の整数IDを返す
+MAX_SAFE_INTEGER = (1 << 53) - 1  # => 9_007_199_254_740_991
+def safe_id(str)
+  Digest::SHA256.hexdigest(str)[0, 16].to_i(16) % MAX_SAFE_INTEGER
 end
 
 while gets
@@ -13,7 +24,7 @@ while gets
   if f['properties'] && f['properties']['筆ID']
     uid_str = "#{pref}_#{basename}_#{f['properties']['筆ID']}"
     f['properties']['global_id'] = uid_str
-    f['id'] = hash64(uid_str)
+    f['id'] = safe_id(uid_str)
   end
   f[:tippecanoe] = {
     :layer => 'fude',
